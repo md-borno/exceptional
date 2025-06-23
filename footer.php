f0803374e63f7fa6fb31a70bd4fb74f2bccbe0db
md-borno <22-46598-1@gmail.com>
2025-06-23 22:20:19 +0600

    Upload file

---
+++ footer.php
@@ -0,0 +1,130 @@
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<div class="container">
+
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_two_third">
+
+						<div class="col_one_third">
+
+							<div class="widget clearfix">
+
+								<img src="images/logo-footer.png" alt="" class="footer-logo">
+
+								<p>We believe in <strong>better Services</strong> &amp; <strong>Education</strong> is free and compulsory for all.</p>
+
+								<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
+									<address>
+										<strong>Office:</strong><br>
+										Kuratoli, Dhaka
+									</address>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> +880 1706-144654<br>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> +880 1521-718721<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> adlulislam95@gmail.com
+								</div>
+
+							</div>
+
+						</div>
+
+						
+
+						
+					</div>
+
+					<div class="col_one_third col_last">
+
+
+						
+							<h5><strong>Mail Us:</strong> Get in touch for upcoming <strong>Services</strong>, <strong>Technologies</strong>  &amp;<strong> Updates</strong>:</h5>
+							
+							<a href="contact.php"><button class="btn btn-success btn-lg btn-block" type="submit">Knock Here</button></a>
+									
+
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+
+							<div class="row">
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a target="_blank" href="https://www.facebook.com/md.borno.92" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+										<i class="icon-facebook"></i>
+										<i class="icon-facebook"></i>
+									</a>
+									<a target="_blank" href="https://www.facebook.com/ExceptionalProgrammers/?ref=bookmarks"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+								</div>
+								<div class="col-md-6 clearfix">
+									<a href="https://md-borno.netlify.app/" class="social-icon si-dark si-colored si-github nobottommargin" style="margin-right: 10px;">
+										<i class="icon-github"></i>
+										<i class="icon-github"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Visit</strong><br>to Portfolio</small></a>
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div><!-- .footer-widgets-wrap end -->
+
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						Copyrights &copy; 2025 All Rights Reserved by MD Borno<br>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+
+							<a target="_blank" href="https://www.facebook.com/md.borno.92" class="social-icon si-small si-borderless si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+							</a>
+							<a target="_blank" href="https://github.com/md-borno" class="social-icon si-small si-borderless si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+
+							
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> adlulislam95@gmail.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> 01706-144654 <span class="middot"></span>
+					</div>
+
+				</div> <!-- End -->
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
